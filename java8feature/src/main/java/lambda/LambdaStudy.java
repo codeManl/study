15d366f7b2cb9a51bb599a73906388566be7a899
liulele <liulele@kayak.com.cn>
2021-09-14 21:06:47 +0800

    2021年9月14日

--- java8feature/src/main/java/lambda/LambdaStudy.java
+++ java8feature/src/main/java/lambda/LambdaStudy.java
@@ -1,4 +1,14 @@
 package lambda;
 
+import java.util.HashMap;
+
 public class LambdaStudy {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("aa","aB");
+        map.put("AA","CD");
+        System.out.println(map.get("aa"));
+        System.out.println(map.get("AA"));
+
+    }
 }
