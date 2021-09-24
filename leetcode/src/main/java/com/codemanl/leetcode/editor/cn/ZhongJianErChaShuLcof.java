1fb0da13d114e0463ace93d90985a56463c0a569
liulele <liulele@kayak.com.cn>
2021-09-24 21:38:27 +0800

    2021年9月24日

--- leetcode/src/main/java/com/codemanl/leetcode/editor/cn/ZhongJianErChaShuLcof.java
+++ leetcode/src/main/java/com/codemanl/leetcode/editor/cn/ZhongJianErChaShuLcof.java
@@ -60,7 +60,7 @@ public class ZhongJianErChaShuLcof {
      * }
      */
     class Solution {
-        HashMap map;
+        HashMap<Integer, Integer> map;
 
         public TreeNode buildTree(int[] preorder, int[] inorder) {
             int length = inorder.length;
@@ -69,10 +69,33 @@ public class ZhongJianErChaShuLcof {
                 map.put(inorder[i], i);
             }
 
-            recursion(preorder, inorder, 0, length - 1, 0, length - 1)
+            return recursion(preorder, inorder, 0, length - 1, 0, length - 1);
         }
 
-        private TreeNode recursion(int[] preorder, int[] inorder, int i, int length, int i1, int length1) {
+        /**
+         * 递归构建二叉树
+         *
+         * @param preorder 前序遍历二叉树
+         * @param inorder  中序遍历二叉树
+         * @param preLeft  前序遍历中的左节点
+         * @param preRight 前序遍历中的右节点
+         * @param inLeft   中序遍历中的左节点
+         * @param inRight  中序遍历中的右节点
+         * @return
+         */
+        private TreeNode recursion(int[] preorder, int[] inorder, int preLeft, int preRight, int inLeft, int inRight) {
+            if (preLeft > preRight) {
+                return null;
+            }
+            int preRoot = preLeft; // 获取根节点在前序遍历中的位置
+            int inRoot = map.get(preorder[preRoot]); // 获取根节点在中序遍历中的位置
+            TreeNode root = new TreeNode(inorder[inRoot]); // 先构建二叉树并指定当前的根节点
+            int inOrder_left_size = inRoot - inLeft; // 获取当前根节点的左边节点数量
+            // 构建左子树并连接到根节点
+            root.left = recursion(preorder, inorder, preLeft + 1, preLeft + inOrder_left_size, inLeft, inRoot - 1);
+            // 构建右子树
+            root.right = recursion(preorder, inorder, preLeft + inOrder_left_size + 1, preRight, inRoot + 1, inRight);
+            return root;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
