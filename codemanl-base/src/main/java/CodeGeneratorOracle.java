eac9bf29dfdbfe3e5d11cb0248618d8cf1201f38
liulele <liulele@kayak.com.cn>
2022-01-27 11:08:34 +0800

    2022年1月27日

--- codemanl-base/src/main/java/CodeGeneratorOracle.java
+++ codemanl-base/src/main/java/CodeGeneratorOracle.java
@@ -14,71 +14,71 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
  * @date 2021/10/19 16:47
  */
 public class CodeGeneratorOracle {
-//    public static void main(String[] args) {
-//        // 1、创建代码生成器
-//        AutoGenerator mpg = new AutoGenerator();
-//
-//        // 2、全局配置
-//        GlobalConfig gc = new GlobalConfig();
-//        String projectPath = System.getProperty("user.dir");
-//        gc.setOutputDir(projectPath + "/src/main/java");
-//        gc.setAuthor("刘乐乐");
-//        //生成后是否打开资源管理器
-//        gc.setOpen(true);
-//        //重新生成时文件是否覆盖
-//        gc.setFileOverride(true);
-//        //去掉Service接口的首字母I
-//        gc.setServiceName("%sService");
-//        //主键策略
-//        gc.setIdType(IdType.ASSIGN_ID);
-//        //定义生成的实体类中日期类型
-//        gc.setDateType(DateType.ONLY_DATE);
-//        //开启Swagger2模式
-//        gc.setSwagger2(false);
-//
-//        mpg.setGlobalConfig(gc);
-//
-//        // 3、数据源配置
-//        DataSourceConfig dsc = new DataSourceConfig();
-//        dsc.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
-//        dsc.setDriverName("oracle.jdbc.OracleDriver");
-//        dsc.setUsername("ljyh");
-//        dsc.setPassword("ljyh");
-//        dsc.setDbType(DbType.ORACLE);
-//        mpg.setDataSource(dsc);
-//
-//        // 4、包配置
-//        PackageConfig pc = new PackageConfig();
-//        // 模块名
-//        pc.setModuleName(null);
-//        pc.setParent("com.kayakwise.etl.finchina");
-//        pc.setController("controller");
-//        pc.setEntity("entity");
-//        pc.setService("service");
-//        pc.setMapper("mapper");
-//        mpg.setPackageInfo(pc);
-//
-//        // 5、策略配置
-//        StrategyConfig strategy = new StrategyConfig();
-//        //对那一张表生成代码  表名用大写
-//        strategy.setInclude("T8_BOND_IMPORTRECORD");
-//        //数据库表映射到实体的命名策略
-//        strategy.setNaming(NamingStrategy.underline_to_camel);
-//        //生成实体时去掉表前缀
-//        strategy.setTablePrefix(pc.getModuleName() + "_");
-//        //数据库表字段映射到实体的命名策略
-//        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-//        // lombok 模型 @Accessors(chain = true) setter链式操作
-//        strategy.setEntityLombokModel(true);
-//        //restful api风格控制器
-//        strategy.setRestControllerStyle(true);
-//        //url中驼峰转连字符
-//        strategy.setControllerMappingHyphenStyle(true);
-//
-//        mpg.setStrategy(strategy);
-//
-//        // 6、执行
-//        mpg.execute();
-//    }
+    public static void main(String[] args) {
+        // 1、创建代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 2、全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("刘乐乐");
+        //生成后是否打开资源管理器
+        gc.setOpen(true);
+        //重新生成时文件是否覆盖
+        gc.setFileOverride(true);
+        //去掉Service接口的首字母I
+        gc.setServiceName("%sService");
+        //主键策略
+        gc.setIdType(IdType.ASSIGN_ID);
+        //定义生成的实体类中日期类型
+        gc.setDateType(DateType.ONLY_DATE);
+        //开启Swagger2模式
+        gc.setSwagger2(false);
+
+        mpg.setGlobalConfig(gc);
+
+        // 3、数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+        dsc.setDriverName("oracle.jdbc.OracleDriver");
+        dsc.setUsername("ljyh");
+        dsc.setPassword("ljyh");
+        dsc.setDbType(DbType.ORACLE);
+        mpg.setDataSource(dsc);
+
+        // 4、包配置
+        PackageConfig pc = new PackageConfig();
+        // 模块名
+        pc.setModuleName(null);
+        pc.setParent("com.kayakwise.etl.finchina");
+        pc.setController("controller");
+        pc.setEntity("entity");
+        pc.setService("service");
+        pc.setMapper("mapper");
+        mpg.setPackageInfo(pc);
+
+        // 5、策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        //对那一张表生成代码  表名用大写
+        strategy.setInclude("T8_BOND_IMPORTRECORD");
+        //数据库表映射到实体的命名策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        //生成实体时去掉表前缀
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        //数据库表字段映射到实体的命名策略
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        // lombok 模型 @Accessors(chain = true) setter链式操作
+        strategy.setEntityLombokModel(true);
+        //restful api风格控制器
+        strategy.setRestControllerStyle(true);
+        //url中驼峰转连字符
+        strategy.setControllerMappingHyphenStyle(true);
+
+        mpg.setStrategy(strategy);
+
+        // 6、执行
+        mpg.execute();
+    }
 
 }
